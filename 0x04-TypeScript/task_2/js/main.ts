c1b5397b0300d3a839de66d830e28b2e88408c96
lkmodibbo <lukmansaidmodibbo@gmail.com>
2025-11-09 13:33:24 +0100

    subject laterals added

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -55,7 +55,6 @@ function executeWork(employee: Director | Teacher): string {
   }
   return employee.workTeacherTasks();
 }
-
 // String literal type
 type Subjects = "Math" | "History";
 
@@ -63,11 +62,8 @@ type Subjects = "Math" | "History";
 export function teachClass(todayClass: Subjects): string {
   if (todayClass === "Math") {
     return "Teaching Math";
-  } else if (todayClass === "History") {
-    return "Teaching History";
-  }
-  // This line is optional because TypeScript already narrows the type
-  return "Unknown class";
+  } 
+  return "Teaching History";
 }
 
 // Example usage
