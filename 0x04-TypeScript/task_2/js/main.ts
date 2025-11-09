dbdd4004c6c8a0477f944c7a2972b807083d7f4f
lkmodibbo <lukmansaidmodibbo@gmail.com>
2025-11-09 12:06:31 +0100

    task_2 director

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -44,3 +44,14 @@ function createEmployee(salary: number | string) {
 console.log(createEmployee(200));
 console.log(createEmployee(1000));
 console.log(createEmployee("500"));
+
+function isDirector(employee: Director | Teacher): employee is Director {
+  return employee instanceof Director;
+}
+
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  }
+  return employee.workTeacherTasks();
+}
