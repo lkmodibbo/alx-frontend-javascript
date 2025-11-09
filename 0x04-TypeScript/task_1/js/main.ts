9eb0f805ba129029359b3cdb99edfeb46dde68da
lkmodibbo <lukmansaidmodibbo@gmail.com>
2025-11-09 07:12:13 +0100

    director interfaces added

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,21 +1,23 @@
-
 interface Teacher {
     readonly firstName: string;
     readonly lastName: string;
     fullTimeEmployee: boolean;
     yearsOfExperience?: number;
     location: string;
-    [key: string]: any;
+    [key: string]: any; // allow any extra attribute
+}
 
+// Director extends Teacher
+interface Directors extends Teacher {
+    numberOfReports: number; // mandatory
 }
 
 const teacher1: Teacher = {
     firstName: "Uncle Science",
-    lastName: "faisal",
+    lastName: "Faisal",
     fullTimeEmployee: true,
     yearsOfExperience: 5,
     location: "Maraba"
-    
 }
 
 const teacher2: Teacher = {
@@ -28,12 +30,21 @@ const teacher2: Teacher = {
 
 const teacher3: Teacher = {
     firstName: "Uncle Bilal",
-    lastName: "faisal",
+    lastName: "Faisal",
     fullTimeEmployee: false,
     location: "Maraba",
     contract: false
 }
 
+const director1: Directors = {
+    firstName: "Lawal",
+    lastName: "Ahmad",
+    location: "Ganye",
+    fullTimeEmployee: true,
+    numberOfReports: 17
+}
+
+// Modifying dynamic attributes
 teacher3.fullTimeEmployee = true;
 teacher3.yearsOfExperience = 2;
 teacher3.contract = true;
@@ -41,13 +52,15 @@ teacher3.contract = true;
 console.log('teacher1', teacher1);
 console.log('teacher2', teacher2);
 console.log('teacher3', teacher3);
-const output = document.getElementById('output')
-if (output) {
-    const pre = document.createElement('pre')
-    pre.textContent = 
-        'teacher1: ' + JSON.stringify(teacher1, null, 2)  + '\n\n' +
-        'teacher2: ' + JSON.stringify(teacher2, null, 2)  + '\n\n' +
-        'teacher3: ' + JSON.stringify(teacher3, null, 2);
-        output.appendChild(pre);
+console.log('director1', director1);
 
+const output = document.getElementById('output');
+if (output) {
+    const pre = document.createElement('pre');
+    pre.textContent =
+        'teacher1: ' + JSON.stringify(teacher1, null, 2) + '\n\n' +
+        'teacher2: ' + JSON.stringify(teacher2, null, 2) + '\n\n' +
+        'teacher3: ' + JSON.stringify(teacher3, null, 2) + '\n\n' +
+        'director1: ' + JSON.stringify(director1, null, 2);
+    output.appendChild(pre);
 }
