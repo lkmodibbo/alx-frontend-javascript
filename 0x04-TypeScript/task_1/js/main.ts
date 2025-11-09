5b0fd0a560f292f8269f34357d4fcea02d193cef
lkmodibbo <lukmansaidmodibbo@gmail.com>
2025-11-09 07:43:07 +0100

    deleted

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -24,7 +24,7 @@ interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 function printTeacher({ firstName, lastName }: { firstName: string; lastName: string }): string {
-  return `${firstName[0]}. ${lastName}`;
+  return `${firstName}. ${lastName}`;
 }
 
 console.log(printTeacher({ firstName: "John", lastName: "Doe" })); // J. Doe
